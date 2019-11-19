57c463f1de488c4946cb96b702b70ad08e16177e
Peter Carmichael <peter.carmichael22@gmail.com>
2019-11-18 22:58:12 -0800

    lab 26 finished

---
+++ src/header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class Header extends React.Component {
+    render() {
+        return <header>
+        <h1>This is the Header for my first React App</h1>
+      </header>
+    }
+}
+
+
+export default Header;
